48ae00cc6f158f4ef943c7e5cf6be0ee636fde14
keimoriyama <keimoriy4ma@gmail.com>
2026-05-25 21:05:02 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -49,17 +49,34 @@ fn extract_head(content: String) -> Option<String> {
     None
 }
 
-fn extract_scuedule_and_type(content: String) -> Option<(String, ScheduleType)> {
+fn extract_scuedule_and_type(content: String) -> Option<Vec<(String, ScheduleType)>> {
     let schedule_re = Regex::new(r"(?P<type>SCHEDULED|DEADLINE):\s+<(?P<date>[^>]+)>").unwrap();
-    if let Some(caps) = schedule_re.captures(&content) {
-        let scheduletype = match &caps["type"] {
-            "SCHEDULED" => ScheduleType::Schedule,
-            "DEADLINE" => ScheduleType::Deadline,
-            _ => return None,
-        };
-        return Some((caps["date"].to_string(), scheduletype));
+    // let mut schedules = Vec::new();
+    let schedules: Vec<(String, ScheduleType)> = schedule_re
+        .find_iter(&content)
+        .map(|matches| {
+            let caps = schedule_re.captures(matches.as_str()).unwrap();
+            let scheduletype = match &caps["type"] {
+                "SCHEDULED" => ScheduleType::Schedule,
+                "DEADLINE" => ScheduleType::Deadline,
+                _ => panic!("Unexpected schedule type"),
+            };
+            (caps["date"].to_string(), scheduletype)
+        })
+        .collect();
+    // if let Some(caps) = schedule_re.captures(&content) {
+    //     let scheduletype = match &caps["type"] {
+    //         "SCHEDULED" => ScheduleType::Schedule,
+    //         "DEADLINE" => ScheduleType::Deadline,
+    //         _ => return None,
+    //     };
+    //     schedules.push((caps["date"].to_string(), scheduletype));
+    // }
+    if schedules.is_empty() {
+        return None;
+    } else {
+        return Some(schedules);
     }
-    None
 }
 
 fn parse_schedules(content: String) -> Vec<Schedule> {
@@ -94,11 +111,40 @@ mod tests {
         assert_eq!(extract_head(sample_input), expected);
     }
     #[test]
-    fn test_extract_head_with_status_None() {
+    fn test_extract_head_with_status_none() {
         let sample_input = " TODO Sample Task".to_string();
         assert_eq!(extract_head(sample_input), None);
     }
 
+    #[test]
+    fn test_extract_schedule_and_type_schedule() {
+        let sample_input = "SCHEDULED: <2024-06-01 Sat 10:00>".to_string();
+        let expected = Some(vec![(
+            "2024-06-01 Sat 10:00".to_string(),
+            ScheduleType::Schedule,
+        )]);
+        assert_eq!(extract_scuedule_and_type(sample_input), expected);
+    }
+    #[test]
+    fn test_extract_schedule_and_type_deadline() {
+        let sample_input = "DEADLINE: <2024-06-01 Sat 10:00>".to_string();
+        let expected = Some(vec![(
+            "2024-06-01 Sat 10:00".to_string(),
+            ScheduleType::Deadline,
+        )]);
+        assert_eq!(extract_scuedule_and_type(sample_input), expected);
+    }
+
+    #[test]
+    fn test_extract_schedule_and_type_both() {
+        let sample_input =
+            "SCHEDULED: <2026-06-05 Fri> DEADLINE: <2026-06-05 Fri 21:00>".to_string();
+        let expected = Some(vec![
+            ("2026-06-05 Fri".to_string(), ScheduleType::Schedule),
+            ("2026-06-05 Fri 21:00".to_string(), ScheduleType::Deadline),
+        ]);
+        assert_eq!(extract_scuedule_and_type(sample_input), expected);
+    }
     #[test]
     fn test_parse_scuedules_scuedule() {
         let sample_input = "* Test Schedule\nSCHEDULED: <2024-06-01 Sat 10:00>".to_string();
